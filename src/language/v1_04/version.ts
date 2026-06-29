82d9a4eb52e40554462791b9faa51720616f1b22
Dan Levitan <support@icc-go.com>
2026-06-29 13:06:55 +0200

    lang: add ICC DSL v1.04 parser

---
+++ src/language/v1_04/version.ts
@@ -0,0 +1,5 @@
+export const LANGUAGE_ID = "icc";
+export const LANGUAGE_NAME = "ICC DSL";
+export const LANGUAGE_VERSION = "1.04.0";
+export const LANGUAGE_VERSION_LABEL = "v1.04";
+export const LANGUAGE_STATUS = "stable";
